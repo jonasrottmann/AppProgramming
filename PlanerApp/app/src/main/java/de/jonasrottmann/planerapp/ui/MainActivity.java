7097bccf3a8978436e0c2f4143b5a3b30513c315
Jonas Rottmann <jonasrottmann@gmail.com>
2017-01-24 18:03:05 +0100

    moved loadermanager into fragment

--- PlanerApp/app/src/main/java/de/jonasrottmann/planerapp/ui/MainActivity.java
+++ PlanerApp/app/src/main/java/de/jonasrottmann/planerapp/ui/MainActivity.java
@@ -3,13 +3,9 @@ package de.jonasrottmann.planerapp.ui;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
 import butterknife.BindView;
@@ -24,7 +20,7 @@ import de.jonasrottmann.planerapp.ui.fragment.OverviewFragment;
  * Created by Jonas Rottmann on 19.01.17.
  * Copyright © 2017 fluidmobile. All rights reserved.
  */
-public class MainActivity extends AppCompatActivity implements OverviewFragment.Contract, DetailFragment.Contract, LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends AppCompatActivity implements OverviewFragment.Contract, DetailFragment.Contract {
     // Views
     @BindView(R.id.container1)
     FrameLayout container1;
@@ -33,15 +29,12 @@ public class MainActivity extends AppCompatActivity implements OverviewFragment.
     FrameLayout container2;
     // Fields
     private boolean isInTwoPaneLayout;
-    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        getSupportLoaderManager().initLoader(R.id.loader, null, this);
 
         // Setup fragments
         getSupportFragmentManager().beginTransaction().replace(R.id.container1, OverviewFragment.getInstance()).commit();
@@ -64,29 +57,6 @@ public class MainActivity extends AppCompatActivity implements OverviewFragment.
             }
             cursor.close();
         }
-    }
-
-    @Override
-    public Cursor getCursor() {
-        return this.cursor;
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        Uri CONTENT_URI = CourseContentProvider.CONTENT_URI;
-        return new CursorLoader(this, CONTENT_URI, null, null, null, null);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        if (OverviewFragment.class.isAssignableFrom(getSupportFragmentManager().findFragmentById(R.id.container1).getClass())) {
-            ((OverviewFragment) getSupportFragmentManager().findFragmentById(R.id.container1)).updateCursor(this.cursor);
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        this.cursor = null;
     }
 
     @Override
--- PlanerApp/app/src/main/java/de/jonasrottmann/planerapp/ui/fragment/OverviewFragment.java
+++ PlanerApp/app/src/main/java/de/jonasrottmann/planerapp/ui/fragment/OverviewFragment.java
@@ -1,9 +1,13 @@
 package de.jonasrottmann.planerapp.ui.fragment;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,21 +18,30 @@ import android.view.ViewGroup;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.jonasrottmann.planerapp.R;
+import de.jonasrottmann.planerapp.data.CourseContentProvider;
+import de.jonasrottmann.planerapp.ui.adapter.CursorRecyclerViewAdapter;
 import de.jonasrottmann.planerapp.ui.adapter.OverviewAdapter;
 
 /**
  * Created by Jonas Rottmann on 19.01.17.
  * Copyright © 2017 fluidmobile. All rights reserved.
  */
-public class OverviewFragment extends ContractFragment<OverviewFragment.Contract> {
+public class OverviewFragment extends ContractFragment<OverviewFragment.Contract> implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @BindView(R.id.recycler)
     RecyclerView recycler;
+    @Nullable
     private Cursor cursor = null;
 
     @NonNull
     public static OverviewFragment getInstance() {
         return new OverviewFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().getSupportLoaderManager().initLoader(R.id.loader, null, this);
     }
 
     @Nullable
@@ -44,26 +57,30 @@ public class OverviewFragment extends ContractFragment<OverviewFragment.Contract
 
         // Setup Views
         recycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-        recycler.setAdapter(new OverviewAdapter(getActivity(), getContract(), getContract().getCursor()));
+        recycler.setAdapter(new OverviewAdapter(getActivity(), getContract(), this.cursor));
 
         return view;
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        this.cursor = getContract().getCursor();
-        ((OverviewAdapter) recycler.getAdapter()).swapCursor(this.cursor);
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri CONTENT_URI = CourseContentProvider.CONTENT_URI;
+        return new CursorLoader(getActivity(), CONTENT_URI, null, null, null, null);
     }
 
-    public void updateCursor(Cursor cursor) {
-        this.cursor = cursor;
-        ((OverviewAdapter) recycler.getAdapter()).swapCursor(this.cursor);
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        this.cursor = data;
+        ((CursorRecyclerViewAdapter) this.recycler.getAdapter()).swapCursor(this.cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        this.cursor = null;
+        ((CursorRecyclerViewAdapter) this.recycler.getAdapter()).swapCursor(this.cursor);
     }
 
     public interface Contract {
         void onCourseClicked(int id);
-
-        Cursor getCursor();
     }
 }
